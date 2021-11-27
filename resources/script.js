24b408550302ddcda0bb38f03b0e15973226c0c6
Abby Henes <abbyhenes@gmail.com>
2021-11-26 21:53:05 -0600

    pseudocode for logic

---
+++ resources/script.js
@@ -0,0 +1,45 @@
+/* 
+Start new game button
+    Removes all .P1 and .P2 classes and innerText
+    Clears P1 and P2 arrays
+
+Player1 turn
+    P1 controls bolded/colored
+    P1 clicks on div, onclick .p1choice is applied to that div
+        Pushes that div id to P1 choice array
+
+Player2 turn
+    P2 clicks on div, onclick .p2choice is applied to that div 
+        Checks if that has already been chosen earlier in the game
+
+Player1 turn
+    onclick .p1choice
+
+Player2 turn
+    onclick .p2choice
+
+Player1 turn
+    onclick .p1choice
+    Checks for winner
+        If innerText of top, center, bottom, left, middle, right, or diagonals = X
+        Log winner to results otherwise...
+
+Player2 turn
+    onclick .p2choice
+    Checks for winner
+
+Player1 turn
+    onclick .p1choice
+    Checks for winner
+
+Player2 turn
+    onclick .p2choice
+    Checks for winner
+
+Player1 turn
+    onclick .p1choice
+    Checks for winner
+    Or tie
+
+
+*/
